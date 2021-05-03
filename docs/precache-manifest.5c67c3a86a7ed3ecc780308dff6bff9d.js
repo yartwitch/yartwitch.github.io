44872ff17b48eceb8987af0f434bd13db4a592ac
yartwitch <mkrtchyan.arsen@gmail.com>
2021-05-03 09:18:44 +0300

    add docs folder

---
+++ docs/precache-manifest.5c67c3a86a7ed3ecc780308dff6bff9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d946529cf523e87de37eafd625324954",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0805874a1fd42ac7bd98",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "652d9d842edfc1e847af",
+    "url": "/static/css/main.5f365171.chunk.css"
+  },
+  {
+    "revision": "0805874a1fd42ac7bd98",
+    "url": "/static/js/2.ed15bc72.chunk.js"
+  },
+  {
+    "revision": "a8eb76e4c6beb72c22bcc9f3c026930b",
+    "url": "/static/js/2.ed15bc72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "652d9d842edfc1e847af",
+    "url": "/static/js/main.473a1e8d.chunk.js"
+  },
+  {
+    "revision": "3fa0e083c040190ae1b8",
+    "url": "/static/js/runtime-main.fb2a6fda.js"
+  }
+]);
